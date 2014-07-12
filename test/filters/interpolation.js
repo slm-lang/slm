267812f7da55b6163936561c5ab3ccbd7b659005
Yury Korolev <yurykorolev@me.com>
2014-07-13 01:35:19 +0400

    Interpolation

--- test/filters/interpolation.js
+++ test/filters/interpolation.js
@@ -36,4 +36,62 @@ suite("Interpolation", function() {
 
     done();
   });
+
+  test('interpolation in text', function(done) {
+    var src =
+      'p\n' +
+      '  . ${this.hello_world} with "quotes"\n' +
+      'p\n' +
+      '  .\n' +
+      '    A message from the compiler: ${this.hello_world}\n';
+
+    assert.deepEqual(
+      template.eval(src, {hello_world: 'Hello World from @env'}),
+      '<p>Hello World from @env with \"quotes\" </p><p>A message from the compiler: Hello World from @env </p>');
+    done();
+  });
+
+  test('interpolation in tag', function(done) {
+    var src = 'p ${this.hello_world}'
+    assert.deepEqual(
+      template.eval(src, {hello_world: 'Hello'}),
+      '<p>Hello</p>');
+    done();
+  });
+
+  test('escape interpolation', function(done) {
+    var src =
+      'p \\${this.hello_world}\n' +
+      'p text1 \\${this.hello_world} text2'
+
+    assert.deepEqual(
+      template.eval(src, {hello_world: 'Hello'}),
+      '<p>${this.hello_world}</p><p>text1 ${this.hello_world} text2</p>');
+    done();
+  });
+
+  test('interpolation with escaping', function(done) {
+    var src ='. ${this.evilMethod()}';
+    assert.deepEqual(
+      template.eval(src, {evilMethod: function() {return '<script>do_something_evil();</script>'}}),
+      '&lt;script&gt;do_something_evil();&lt;/script&gt; ');
+    done();
+  });
+
+  test('interpolation without escaping', function(done) {
+    var src ='| ${= this.evilMethod()}';
+    assert.deepEqual(
+      template.eval(src, {evilMethod: function() {return '<script>do_something_evil();</script>'}}),
+      '<script>do_something_evil();</script>');
+    done();
+  });
+
+  test('interpolation with escaping and delimiter', function(done) {
+    var src ='| ${(this.evilMethod())}';
+    assert.deepEqual(
+      template.eval(src, {evilMethod: function() {return '<script>do_something_evil();</script>'}}),
+      '&lt;script&gt;do_something_evil();&lt;/script&gt;');
+
+    done();
+  });
 });
