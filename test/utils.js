332561290f8a6319cc6db9c807fbb9fc2a333561
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2014-07-17 23:01:47 +0400

    Some work on flatten

--- test/utils.js
+++ test/utils.js
@@ -10,7 +10,28 @@ var assert  = Lab.assert
 suite("Utils", function() {
 
   test("rejectEmpty", function(done) {
-    assert.deepEqual(Utils.rejectEmpty(['a', null, 'b', '', 'c']), ['a', 'b', 'c']);
+    assert.deepEqual(
+      Utils.rejectEmpty(['a', null, 'b', '', 'c']),
+      ['a', 'b', 'c']
+    );
+    done();
+  });
+
+  test("flatten", function(done) {
+    assert.deepEqual(
+      Utils.flatten([1, [2, 3], [4, [5, [6, 7]]]]),
+      ['1', '2', '3', '4', '5', '6', '7']
+    );
+
+    assert.deepEqual(
+      Utils.flatten([1, 2, [3], [4, [5, 6], 7], [8, 9]]),
+      ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    );
+
+    assert.deepEqual(
+      Utils.flatten([1, 2, [], [3, [4, 5, []], 6]]),
+      ['1', '2', '3', '4', '5', '6']
+    );
     done();
   });
 });
