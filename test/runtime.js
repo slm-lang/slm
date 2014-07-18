ee10aa242c84827bdcbed47ea9d71c57597c4af7
Yury Korolev <yurykorolev@me.com>
2014-07-18 13:36:43 +0400

    Rename utils module to runtime

--- test/runtime.js
+++ test/runtime.js
@@ -1,5 +1,5 @@
 var Lab   = require('lab'),
-    Utils = require('../lib/utils');
+    Runtime = require('../lib/runtime');
 
 var suite   = Lab.experiment;
 var before  = Lab.before;
@@ -7,11 +7,11 @@ var after   = Lab.after;
 var test    = Lab.test
 var assert  = Lab.assert
 
-suite("Utils", function() {
+suite("Runtime", function() {
 
   test("rejectEmpty", function(done) {
     assert.deepEqual(
-      Utils.rejectEmpty(['a', null, 'b', '', 'c']),
+      Runtime.rejectEmpty(['a', null, 'b', '', 'c']),
       ['a', 'b', 'c']
     );
     done();
@@ -19,17 +19,17 @@ suite("Utils", function() {
 
   test("flatten", function(done) {
     assert.deepEqual(
-      Utils.flatten([1, [2, 3], [4, [5, [6, 7]]]]),
+      Runtime.flatten([1, [2, 3], [4, [5, [6, 7]]]]),
       ['1', '2', '3', '4', '5', '6', '7']
     );
 
     assert.deepEqual(
-      Utils.flatten([1, 2, [3], [4, [5, 6], 7], [8, 9]]),
+      Runtime.flatten([1, 2, [3], [4, [5, 6], 7], [8, 9]]),
       ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     );
 
     assert.deepEqual(
-      Utils.flatten([1, 2, [], [3, [4, 5, []], 6]]),
+      Runtime.flatten([1, 2, [], [3, [4, 5, []], 6]]),
       ['1', '2', '3', '4', '5', '6']
     );
     done();
