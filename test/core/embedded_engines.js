846cf905b3b87325a0f66970c53d419d74192a11
Yury Korolev <yurykorolev@me.com>
2014-07-30 12:54:34 +0400

    Added embedded engines support (css, javascript for now)

---
+++ test/core/embedded_engines.js
@@ -0,0 +1,62 @@
+var Lab = require('lab'),
+    Template = require('../../lib/template'),
+    assertHtml = require('../helper').assertHtml;
+
+var suite   = Lab.experiment;
+var before  = Lab.before;
+var after   = Lab.after;
+var test    = Lab.test
+var assert  = Lab.assert
+
+suite('Embedded engines', function() {
+  var template;
+  before(function(done) {
+    template = new Template
+    done();
+  });
+
+  test('render with javascript', function(done) {
+    assertHtml(template, [
+      'javascript:   ',
+      '  $(function() {});',
+      '',
+      '',
+      '  alert(\'hello\')',
+      'p Hi',
+      ],
+      '<script type="text/javascript">$(function() {});\n\n\nalert(\'hello\')</script><p>Hi</p>',
+      {}, done);
+  });
+
+  test('render with script', function(done) {
+    assertHtml(template, [
+      'script:   ',
+      '  $(function() {});',
+      '',
+      '',
+      '  alert(\'hello\')',
+      'p Hi',
+      ],
+      '<script type="text/javascript">$(function() {});\n\n\nalert(\'hello\')</script><p>Hi</p>',
+      {}, done);
+  });
+
+  test('render with javascript including variable', function(done) {
+    assertHtml(template, [
+      '- var func = "alert(\'hello\');"',
+      'javascript:   ',
+      '  $(function() { ${func} });',
+      ],
+      '<script type="text/javascript">$(function() { alert(\'hello\'); });</script>',
+      {}, done);
+  });
+
+  test('render with css', function(done) {
+    assertHtml(template, [
+      'css:',
+      '  body { color: red; }',
+      ],
+      '<style type="text/css">body { color: red; }</style>',
+      {}, done);
+  });
+});
