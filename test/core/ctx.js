a6cff07c84fb9a701b7e0f09b1e68da6dd45cdd2
Yury Korolev <yurykorolev@me.com>
2014-08-01 15:19:01 +0400

    Fix bug if template is cached

---
+++ test/core/ctx.js
@@ -0,0 +1,130 @@
+var Lab = require('lab'),
+    Template = require('../../lib/template'),
+    Ctx = require('../../lib/ctx');
+
+var suite   = Lab.experiment;
+var before  = Lab.before;
+var after   = Lab.after;
+var test    = Lab.test
+var assert  = Lab.assert
+
+suite('Ctx', function() {
+  var template;
+  before(function(done) {
+    template = new Template
+    done();
+  });
+
+  test('extend with same path', function(done) {
+      Ctx.cache = {};
+
+      Ctx.cache['layout.slm'] = template.exec([
+        'html',
+        '  head',
+        '    = content("head")',
+        '  body',
+        '    = content()'
+        ].join('\n'))
+
+      var src = [
+        '- extend("layout")',
+        '= content("head");',
+        '  meta name="keywords" content=this.who',
+        'p Hello, ${this.who}'
+      ].join('\n')
+
+      var ctx = new Ctx();
+      ctx.filename = 'view.slm'
+
+      var result = template.eval(src, {who: 'World', what: 'the best'}, {}, ctx);
+      assert.deepEqual(result, '<html><head><meta content="World" name="keywords" /></head><body><p>Hello, World</p></body></html>');
+      done();
+  });
+
+  test('extend with abs path', function(done) {
+      Ctx.cache = {};
+
+      Ctx.cache['/layout.slm'] = template.exec([
+        'html',
+        '  head',
+        '    = content("head")',
+        '  body',
+        '    = content()'
+        ].join('\n'))
+
+      var src = [
+        '- extend("/layout")',
+        '= content("head");',
+        '  meta name="keywords" content=this.who',
+        'p Hello, ${this.who}'
+      ].join('\n')
+
+      var ctx = new Ctx();
+      ctx.filename = '/views/view';
+      ctx.basePath = '/';
+
+      var result = template.eval(src, {who: 'World', what: 'the best'}, {}, ctx);
+      assert.deepEqual(result, '<html><head><meta content="World" name="keywords" /></head><body><p>Hello, World</p></body></html>');
+      done();
+  });
+
+  test('extend with same nested path', function(done) {
+      Ctx.cache = {};
+
+      Ctx.cache['/views/layout.slm'] = template.exec([
+        'html',
+        '  head',
+        '    = content("head")',
+        '  body',
+        '    = content()'
+        ].join('\n'))
+
+      var src = [
+        '- extend("layout")',
+        '= content("head");',
+        '  meta name="keywords" content=this.who',
+        'p Hello, ${this.who}'
+      ].join('\n')
+
+      var ctx = new Ctx();
+      ctx.filename = '/views/view.slm';
+      ctx.basePath = '/';
+
+      var result = template.eval(src, {who: 'World', what: 'the best'}, {}, ctx);
+      assert.deepEqual(result, '<html><head><meta content="World" name="keywords" /></head><body><p>Hello, World</p></body></html>');
+      done();
+  });
+
+  test('extend with same nested path 2', function(done) {
+      Ctx.cache = {};
+
+      Ctx.cache['/views/layouts/app.slm'] = template.exec([
+        'html',
+        '  head',
+        '    = content("head")',
+        '  body',
+        '    = content()'
+        ].join('\n'))
+
+      Ctx.cache['/views/products/form.slm'] = template.exec([
+        'form',
+        '  input type="submit"',
+        ].join('\n'))
+
+
+      var src = [
+        '- extend("../layouts/app")',
+        '= content("head");',
+        '  meta name="keywords" content=this.who',
+        '= partial("form", this)'
+      ].join('\n')
+
+      var ctx = new Ctx();
+      ctx.filename = '/views/products/new.slm';
+      ctx.basePath = '/views';
+
+      var result = template.eval(src, {who: 'World', what: 'the best'}, {}, ctx);
+      assert.deepEqual(result, '<html><head><meta content="World" name="keywords" /></head><body><form><input type="submit" /></form></body></html>');
+      done();
+  });
+});
