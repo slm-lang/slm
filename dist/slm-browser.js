05f782c7c30ec49fa406533a77f267038c6cfff5
Yury Korolev <yurykorolev@me.com>
2014-12-30 00:29:37 +0300

    rebuild and add gz to ignore

--- dist/slm-browser.js
+++ dist/slm-browser.js
@@ -78,12 +78,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	var ControlFlow = __webpack_require__(10);
 	var Controls = __webpack_require__(11);
 	var Embeddeds = __webpack_require__(12);
-	var Engine = __webpack_require__(3);
+	var Engine = __webpack_require__(4);
 	var Escape = __webpack_require__(13);
 	var FastHtml = __webpack_require__(17);
 	var Interpolate = __webpack_require__(14);
 	var MultiFlattener = __webpack_require__(15);
-	var Parser = __webpack_require__(4);
+	var Parser = __webpack_require__(5);
 	var StaticMerger = __webpack_require__(16);
 	var StringGenerator = __webpack_require__(18);
 
@@ -187,7 +187,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var Runtime = __webpack_require__(5);
+	var Runtime = __webpack_require__(3);
 
 	function BrowserCtx() {};
 
@@ -240,6 +240,183 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
+	var ampRe = /&/g;
+	var escapeRe = /[&<>"]/;
+	var gtRe = />/g;
+	var ltRe = /</g;
+	var quotRe = /"/g;
+
+	function safe(val) {
+	  if (!val || val.htmlSafe) {
+	    return val;
+	  }
+
+	  var res = new String(val);
+	  res.htmlSafe = true;
+	  return res;
+	}
+
+	function escape(str) {
+	  if (typeof str !== 'string') {
+	    if (!str) {
+	      return '';
+	    }
+	    if (str.htmlSafe) {
+	      return str.toString();
+	    }
+	    str = str.toString();
+	  }
+
+	  if (escapeRe.test(str) ) {
+	    if (str.indexOf('&') !== -1) {
+	      str = str.replace(ampRe, '&amp;');
+	    }
+	    if (str.indexOf('<') !== -1) {
+	      str = str.replace(ltRe, '&lt;');
+	    }
+	    if (str.indexOf('>') !== -1) {
+	      str = str.replace(gtRe, '&gt;');
+	    }
+	    if (str.indexOf('"') !== -1) {
+	      str = str.replace(quotRe, '&quot;');
+	    }
+	  }
+
+	  return str;
+	}
+
+	function rejectEmpty(arr) {
+	  var res = [];
+
+	  for (var i = 0, l = arr.length; i < l; i++) {
+	    var el = arr[i];
+	    if (el !== null && el.length) {
+	      res.push(el);
+	    }
+	  }
+
+	  return res;
+	}
+
+	function flatten(arr) {
+	  return arr.reduce(function (acc, val) {
+	    if (val === null) {
+	      return acc;
+	    }
+	    return acc.concat(val.constructor === Array ? flatten(val) : val.toString());
+	  }, []);
+	}
+
+	function Ctx() {
+	  this.reset();
+	  this.template = this.basePath = null;
+	}
+
+	Ctx.cache = {};
+
+	var CtxProto = Ctx.prototype;
+
+	CtxProto.rebind = function() {
+	  this._content = this.content.bind(this);
+	  this._extend = this.extend.bind(this);
+	  this._partial = this.partial.bind(this);
+	  this._defaultContent = this.defaultContent.bind(this);
+	  this._append = this.append.bind(this);
+	  this._prepend = this.prepend.bind(this);
+	};
+
+	/*
+	  Prepare ctx for next template rendering
+	*/
+	CtxProto.reset = function() {
+	  this._contents = {};
+	  this.res = '';
+	  this.stack = [];
+	  this.m = null;
+	};
+
+	/*
+	  Pop stack to sp
+	*/
+	CtxProto.pop = function(sp) {
+	  var l = this.stack.length;
+	  var filename = this.filename;
+	  while (sp < l--) {
+	    var path = this._resolvePath(this.stack.pop());
+	    var fn = this._load(path);
+	    this.filename = path;
+	    fn.call(this.m, this);
+	  }
+	  this.filename = filename;
+	  return this.res;
+	};
+
+	CtxProto.partial = function(path, model, cb) {
+	  if (cb) {
+	    this.res = cb.call(this.m, this);
+	  }
+
+	  path = this._resolvePath(path);
+
+	  var f = this._load(path), oldModel = this.m, filename = this.filename;
+	  this.filename = path;
+	  var res = safe(f.call(this.m = model, this));
+	  this.m = oldModel;
+	  this.filename = filename;
+	  return res;
+	};
+
+	CtxProto.extend = function(path) {
+	  this.stack.push(path);
+	};
+
+	CtxProto.content = function() {
+	  switch(arguments.length) {
+	    case 0:
+	      return safe(this.res);
+	    case 1:
+	      return this._contents[arguments[0]] || '';
+	    case 2:
+	      var name = arguments[0], cb = arguments[1];
+	      if (name) {
+	        // capturing block
+	        this._contents[name] = cb.call(this.m);
+	        return '';
+	      } else {
+	        return cb.call(this.m);
+	      }
+	  }
+	};
+
+	CtxProto.defaultContent = function(name, cb) {
+	  return this._contents[name] || cb.call(this.m);
+	};
+
+	CtxProto.append = function(name, cb) {
+	  var contents = this._contents[name] || '';
+	  this._contents[name] = contents + cb.call(this.m);
+	  return '';
+	};
+
+	CtxProto.prepend = function(name, cb) {
+	  var contents = this._contents[name] || '';
+	  this._contents[name] = cb.call(this.m) + contents;
+	  return '';
+	};
+
+	module.exports = {
+	  Ctx: Ctx,
+	  escape: escape,
+	  flatten: flatten,
+	  rejectEmpty: rejectEmpty,
+	  safe: safe
+	};
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
 	function Engine() {
 	  this._chain = [];
 	}
@@ -263,7 +440,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 4 */
+/* 5 */
 /***/ function(module, exports, __webpack_require__) {
 
 	
@@ -941,183 +1118,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 5 */
-/***/ function(module, exports, __webpack_require__) {
-
-	var ampRe = /&/g;
-	var escapeRe = /[&<>"]/;
-	var gtRe = />/g;
-	var ltRe = /</g;
-	var quotRe = /"/g;
-
-	function safe(val) {
-	  if (!val || val.htmlSafe) {
-	    return val;
-	  }
-
-	  var res = new String(val);
-	  res.htmlSafe = true;
-	  return res;
-	}
-
-	function escape(str) {
-	  if (typeof str !== 'string') {
-	    if (!str) {
-	      return '';
-	    }
-	    if (str.htmlSafe) {
-	      return str.toString();
-	    }
-	    str = str.toString();
-	  }
-
-	  if (escapeRe.test(str) ) {
-	    if (str.indexOf('&') !== -1) {
-	      str = str.replace(ampRe, '&amp;');
-	    }
-	    if (str.indexOf('<') !== -1) {
-	      str = str.replace(ltRe, '&lt;');
-	    }
-	    if (str.indexOf('>') !== -1) {
-	      str = str.replace(gtRe, '&gt;');
-	    }
-	    if (str.indexOf('"') !== -1) {
-	      str = str.replace(quotRe, '&quot;');
-	    }
-	  }
-
-	  return str;
-	}
-
-	function rejectEmpty(arr) {
-	  var res = [];
-
-	  for (var i = 0, l = arr.length; i < l; i++) {
-	    var el = arr[i];
-	    if (el !== null && el.length) {
-	      res.push(el);
-	    }
-	  }
-
-	  return res;
-	}
-
-	function flatten(arr) {
-	  return arr.reduce(function (acc, val) {
-	    if (val === null) {
-	      return acc;
-	    }
-	    return acc.concat(val.constructor === Array ? flatten(val) : val.toString());
-	  }, []);
-	}
-
-	function Ctx() {
-	  this.reset();
-	  this.template = this.basePath = null;
-	}
-
-	Ctx.cache = {};
-
-	var CtxProto = Ctx.prototype;
-
-	CtxProto.rebind = function() {
-	  this._content = this.content.bind(this);
-	  this._extend = this.extend.bind(this);
-	  this._partial = this.partial.bind(this);
-	  this._defaultContent = this.defaultContent.bind(this);
-	  this._append = this.append.bind(this);
-	  this._prepend = this.prepend.bind(this);
-	};
-
-	/*
-	  Prepare ctx for next template rendering
-	*/
-	CtxProto.reset = function() {
-	  this._contents = {};
-	  this.res = '';
-	  this.stack = [];
-	  this.m = null;
-	};
-
-	/*
-	  Pop stack to sp
-	*/
-	CtxProto.pop = function(sp) {
-	  var l = this.stack.length;
-	  var filename = this.filename;
-	  while (sp < l--) {
-	    var path = this._resolvePath(this.stack.pop());
-	    var fn = this._load(path);
-	    this.filename = path;
-	    fn.call(this.m, this);
-	  }
-	  this.filename = filename;
-	  return this.res;
-	};
-
-	CtxProto.partial = function(path, model, cb) {
-	  if (cb) {
-	    this.res = cb.call(this.m, this);
-	  }
-
-	  path = this._resolvePath(path);
-
-	  var f = this._load(path), oldModel = this.m, filename = this.filename;
-	  this.filename = path;
-	  var res = safe(f.call(this.m = model, this));
-	  this.m = oldModel;
-	  this.filename = filename;
-	  return res;
-	};
-
-	CtxProto.extend = function(path) {
-	  this.stack.push(path);
-	};
-
-	CtxProto.content = function() {
-	  switch(arguments.length) {
-	    case 0:
-	      return safe(this.res);
-	    case 1:
-	      return this._contents[arguments[0]] || '';
-	    case 2:
-	      var name = arguments[0], cb = arguments[1];
-	      if (name) {
-	        // capturing block
-	        this._contents[name] = cb.call(this.m);
-	        return '';
-	      } else {
-	        return cb.call(this.m);
-	      }
-	  }
-	};
-
-	CtxProto.defaultContent = function(name, cb) {
-	  return this._contents[name] || cb.call(this.m);
-	};
-
-	CtxProto.append = function(name, cb) {
-	  var contents = this._contents[name] || '';
-	  this._contents[name] = contents + cb.call(this.m);
-	  return '';
-	};
-
-	CtxProto.prepend = function(name, cb) {
-	  var contents = this._contents[name] || '';
-	  this._contents[name] = cb.call(this.m) + contents;
-	  return '';
-	};
-
-	module.exports = {
-	  Ctx: Ctx,
-	  escape: escape,
-	  flatten: flatten,
-	  rejectEmpty: rejectEmpty,
-	  safe: safe
-	};
-
-
-/***/ },
 /* 6 */
 /***/ function(module, exports, __webpack_require__) {
 
@@ -1567,7 +1567,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports, __webpack_require__) {
 
 	var Filter = __webpack_require__(21);
-	var Runtime = __webpack_require__(5);
+	var Runtime = __webpack_require__(3);
 
 	function Escape() {
 	  this._disableEscape = false;
