9bbf34f01f248f1ccb2e937a61759c4f31eea5c7
Yury Korolev <yury.korolev@gmail.com>
2016-12-15 23:45:58 +0300

    Migrate vm tests

--- __tests__/vm.js
+++ __tests__/vm.js
@@ -1,58 +1,40 @@
-var Lab = require('lab');
 var VM = require('../lib/vm');
-var assert  = require('chai').assert;
 
-var lab = exports.lab = Lab.script();
-
-lab.experiment('VM', function() {
+describe('VM', function() {
 
   var vm = new VM();
 
-  lab.test('.rejectEmpty()', function(done) {
-    assert.deepEqual(
-      vm.rejectEmpty(['a', null, 'b', '', 'c']),
-      ['a', 'b', 'c']
-    );
-    done();
+  it('.rejectEmpty()', function() {
+    expect(vm.rejectEmpty(['a', null, 'b', '', 'c'])).toEqual(['a', 'b', 'c']);
   });
 
-  lab.test('.flatten()', function(done) {
-    assert.deepEqual(
-      vm.flatten([1, [2, 3], [4, [5, [6, 7]]]]),
-      ['1', '2', '3', '4', '5', '6', '7']
-    );
+  it('.flatten()', function() {
+    expect(vm.flatten([1, [2, 3], [4, [5, [6, 7]]]]))
+    .toEqual(['1', '2', '3', '4', '5', '6', '7']);
 
-    assert.deepEqual(
-      vm.flatten([1, 2, [3], [4, [5, 6], 7], [8, 9]]),
-      ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    );
+    expect(vm.flatten([1, 2, [3], [4, [5, 6], 7], [8, 9]]))
+    .toEqual(['1', '2', '3', '4', '5', '6', '7', '8', '9']);
 
-    assert.deepEqual(
-      vm.flatten([1, 2, [], [3, [4, 5, []], 6]]),
-      ['1', '2', '3', '4', '5', '6']
-    );
-    done();
+    expect(vm.flatten([1, 2, [], [3, [4, 5, []], 6]]))
+    .toEqual(['1', '2', '3', '4', '5', '6']);
   });
 
-  lab.test('.escape()', function(done) {
-    assert.deepEqual(vm.escape(), '');
-    assert.deepEqual(vm.escape(null), '');
-    assert.deepEqual(vm.escape(' '), ' ');
-    assert.deepEqual(vm.escape('<'), '&lt;');
-    assert.deepEqual(vm.escape('>'), '&gt;');
-    assert.deepEqual(vm.escape('"'), '&quot;');
-    assert.deepEqual(vm.escape('&'), '&amp;');
-    assert.deepEqual(vm.escape('<javascript>alert("alert!")</javascript>'), '&lt;javascript&gt;alert(&quot;alert!&quot;)&lt;/javascript&gt;');
-
-    done();
+  it('.escape()', function() {
+    expect(vm.escape()).toEqual('');
+    expect(vm.escape(null)).toEqual('');
+    expect(vm.escape(' ')).toEqual(' ');
+    expect(vm.escape('<')).toEqual('&lt;');
+    expect(vm.escape('>')).toEqual('&gt;');
+    expect(vm.escape('"')).toEqual('&quot;');
+    expect(vm.escape('&')).toEqual('&amp;');
+    expect(vm.escape('<javascript>alert("alert!")</javascript>'))
+      .toEqual('&lt;javascript&gt;alert(&quot;alert!&quot;)&lt;/javascript&gt;');
   });
 
-  lab.test('.safe()', function(done) {
-    assert.deepEqual(vm.escape(vm.safe('<javascript>alert("alert!")</javascript>')), '<javascript>alert("alert!")</javascript>');
-    assert.deepEqual(vm.escape(vm.safe('')), '');
-    assert.deepEqual(vm.escape(vm.safe()), '');
-    assert.deepEqual(vm.escape(vm.safe(null)), '');
-
-    done();
+  it('.safe()', function() {
+    expect(vm.escape(vm.safe('<javascript>alert("alert!")</javascript>'))).toEqual('<javascript>alert("alert!")</javascript>');
+    expect(vm.escape(vm.safe(''))).toEqual('');
+    expect(vm.escape(vm.safe())).toEqual('');
+    expect(vm.escape(vm.safe(null))).toEqual('');
   });
 });
