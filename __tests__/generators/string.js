8521db3b71a29ebc0f33c2f0597c0f3538c173d4
Yury Korolev <yury.korolev@gmail.com>
2018-04-02 10:36:31 +0300

    Update dev packages

--- __tests__/generators/string.js
+++ __tests__/generators/string.js
@@ -3,29 +3,34 @@ var Generator = require('../../lib/generators/string');
 describe('String generator', function() {
   var generator = null;
 
-  beforeEach(function() { generator = new Generator(); });
+  beforeEach(function() {
+    generator = new Generator();
+  });
 
   test('compiles simple expressions', function() {
-    expect(generator.exec(['static', 'test'])).toEqual('var _b=\'\';_b+="test";');
-    expect(generator.exec(['dynamic', 'test'])).toEqual('var _b=\'\';_b+=test;');
-    expect(generator.exec(['code', 'test'])).toEqual('var _b=\'\';test');
+    expect(generator.exec(['static', 'test'])).toEqual(
+      'var _b=\'\';_b+="test";',
+    );
+    expect(generator.exec(['dynamic', 'test'])).toEqual("var _b='';_b+=test;");
+    expect(generator.exec(['code', 'test'])).toEqual("var _b='';test");
   });
 
   test('compiles multi expression', function() {
-    expect(generator.exec(['multi',
-      ['static', 'static'],
-      ['dynamic', 'dynamic'],
-      ['code', 'code']])
-    ,).toEqual(
-      'var _b=\'\';_b+="static";\n_b+=dynamic;\ncode'
-    );
+    expect(
+      generator.exec([
+        'multi',
+        ['static', 'static'],
+        ['dynamic', 'dynamic'],
+        ['code', 'code'],
+      ]),
+    ).toEqual('var _b=\'\';_b+="static";\n_b+=dynamic;\ncode');
   });
 
-  expect('throws an error on unknown expression', function() {
+  test('throws an error on unknown expression', function() {
     expect(function() {
-      generator.exec(['multi',
-        ['unknown', 'static'],
-        ['code', 'code']]);
-    }).toThrowError('Generator supports only core expressions - found ["unknown","static"]');
+      generator.exec(['multi', ['unknown', 'static'], ['code', 'code']]);
+    }).toThrowError(
+      'Generator supports only core expressions - found ["unknown","static"]',
+    );
   });
 });
--- __tests__/vm_node.js
+++ __tests__/vm_node.js
@@ -14,70 +14,104 @@ describe('VMNode', function() {
 
   test('extend with same path', function() {
     var options = {
-      basePath: '/'
+      basePath: '/',
     };
 
     options.filename = '/layout.slm';
-    fixture.vm.cache(options.filename, fixture.template.exec([
-      'html',
-      '  head',
-      '    = content("head")',
-      '  body',
-      '    = content()'
-    ].join('\n'), options, fixture.vm));
+    fixture.vm.cache(
+      options.filename,
+      fixture.template.exec(
+        [
+          'html',
+          '  head',
+          '    = content("head")',
+          '  body',
+          '    = content()',
+        ].join('\n'),
+        options,
+        fixture.vm,
+      ),
+    );
 
     options.filename = '/view.slm';
     var src = [
       '- extend("layout")',
       '= content("head")',
       '  meta name="keywords" content=this.who',
-      'p Hello, ${this.who}'
+      'p Hello, ${this.who}',
     ].join('\n');
 
-
-    var result = fixture.template.render(src, {who: 'World', what: 'the best'}, options, fixture.vm);
-    expect(result).toEqual('<html><head><meta content="World" name="keywords" /></head><body><p>Hello, World</p></body></html>');
+    var result = fixture.template.render(
+      src,
+      { who: 'World', what: 'the best' },
+      options,
+      fixture.vm,
+    );
+    expect(result).toEqual(
+      '<html><head><meta content="World" name="keywords" /></head><body><p>Hello, World</p></body></html>',
+    );
   });
 
   test('extend with abs path', function() {
     var options = {
-      basePath: '/views'
+      basePath: '/views',
     };
 
     options.filename = '/views/layout.slm';
-    fixture.vm.cache('/views/layout.slm', fixture.template.exec([
-      'html',
-      '  head',
-      '    = content("head")',
-      '  body',
-      '    = content()'
-    ].join('\n'), options, fixture.vm));
+    fixture.vm.cache(
+      '/views/layout.slm',
+      fixture.template.exec(
+        [
+          'html',
+          '  head',
+          '    = content("head")',
+          '  body',
+          '    = content()',
+        ].join('\n'),
+        options,
+        fixture.vm,
+      ),
+    );
 
     options.filename = '/views/view';
     var src = [
       '- extend("/layout")',
       '= content("head");',
       '  meta name="keywords" content=this.who',
-      'p Hello, ${this.who}'
+      'p Hello, ${this.who}',
     ].join('\n');
 
-    var result = fixture.template.render(src, {who: 'World', what: 'the best'}, options, fixture.vm);
-    expect(result).toEqual('<html><head><meta content="World" name="keywords" /></head><body><p>Hello, World</p></body></html>');
+    var result = fixture.template.render(
+      src,
+      { who: 'World', what: 'the best' },
+      options,
+      fixture.vm,
+    );
+    expect(result).toEqual(
+      '<html><head><meta content="World" name="keywords" /></head><body><p>Hello, World</p></body></html>',
+    );
   });
 
-  expect('extend with same nested path', function() {
+  test('extend with same nested path', function() {
     var options = {
-      basePath: '/'
+      basePath: '/',
     };
 
     options.filename = '/views/layout.slm';
-    fixture.vm.cache(options.filename, fixture.template.exec([
-      'html',
-      '  head',
-      '    = content("head")',
-      '  body',
-      '    = content()'
-    ].join('\n'), {}, fixture.vm));
+    fixture.vm.cache(
+      options.filename,
+      fixture.template.exec(
+        [
+          'html',
+          '  head',
+          '    = content("head")',
+          '  body',
+          '    = content()',
+        ].join('\n'),
+        {},
+        fixture.vm,
+      ),
+    );
 
     options.filename = '/views/view.slm';
 
@@ -85,31 +119,49 @@ describe('VMNode', function() {
       '- extend("layout")',
       '= content("head");',
       '  meta name="keywords" content=this.who',
-      'p Hello, ${this.who}'
+      'p Hello, ${this.who}',
     ].join('\n');
 
-    var result = fixture.template.render(src, {who: 'World', what: 'the best'}, options, fixture.vm);
-    expect(result).toEqual('<html><head><meta content="World" name="keywords" /></head><body><p>Hello, World</p></body></html>');
+    var result = fixture.template.render(
+      src,
+      { who: 'World', what: 'the best' },
+      options,
+      fixture.vm,
+    );
+    expect(result).toEqual(
+      '<html><head><meta content="World" name="keywords" /></head><body><p>Hello, World</p></body></html>',
+    );
   });
 
   test('extend with same nested path 2', function() {
     var options = {
-      basePath: '/views'
+      basePath: '/views',
     };
     options.filename = '/views/layouts/app.slm';
-    fixture.vm.cache(options.filename, fixture.template.exec([
-      'html',
-      '  head',
-      '    = content("head")',
-      '  body',
-      '    = content()'
-    ].join('\n'), options, fixture.vm));
+    fixture.vm.cache(
+      options.filename,
+      fixture.template.exec(
+        [
+          'html',
+          '  head',
+          '    = content("head")',
+          '  body',
+          '    = content()',
+        ].join('\n'),
+        options,
+        fixture.vm,
+      ),
+    );
 
     options.filename = '/views/products/form.slm';
-    fixture.vm.cache(options.filename, fixture.template.exec([
-      'form',
-      '  input type="submit"'
-    ].join('\n'), options, fixture.vm));
+    fixture.vm.cache(
+      options.filename,
+      fixture.template.exec(
+        ['form', '  input type="submit"'].join('\n'),
+        options,
+        fixture.vm,
+      ),
+    );
 
     options.filename = '/views/products/new.slm';
 
@@ -117,24 +169,30 @@ describe('VMNode', function() {
       '- extend("../layouts/app")',
       '= content("head");',
       '  meta name="keywords" content=this.who',
-      '= partial("form", this)'
+      '= partial("form", this)',
     ].join('\n');
 
-    var result = fixture.template.render(src, {who: 'World', what: 'the best'}, options, fixture.vm);
-    expect(result).toEqual('<html><head><meta content="World" name="keywords" /></head><body><form><input type="submit" /></form></body></html>');
+    var result = fixture.template.render(
+      src,
+      { who: 'World', what: 'the best' },
+      options,
+      fixture.vm,
+    );
+    expect(result).toEqual(
+      '<html><head><meta content="World" name="keywords" /></head><body><form><input type="submit" /></form></body></html>',
+    );
   });
 
   test('test require', function() {
     var options = {
       basePath: '/views',
-      require: module.require
+      require: module.require,
     };
 
     options.filename = '/views/forms/form.slm';
-    var src = [
-      '- var p = require("path")',
-      'p = p.extname("super.slm")'
-    ].join('\n');
+    var src = ['- var p = require("path")', 'p = p.extname("super.slm")'].join(
+      '\n',
+    );
 
     var result = fixture.template.render(src, {}, options, fixture.vm);
     expect(result).toEqual('<p>.slm</p>');
@@ -142,82 +200,100 @@ describe('VMNode', function() {
 
   test('test content default', function() {
     var options = {
-      basePath: '/views'
+      basePath: '/views',
     };
 
     options.filename = '/views/layouts/app.slm';
 
-    fixture.vm.cache(options.filename, fixture.template.exec([
-      'html',
-      '  head',
-      '    = content("title", "default")',
-      '      title Default title',
-      '  body',
-      '    = content()'
-    ].join('\n'), options, fixture.vm));
+    fixture.vm.cache(
+      options.filename,
+      fixture.template.exec(
+        [
+          'html',
+          '  head',
+          '    = content("title", "default")',
+          '      title Default title',
+          '  body',
+          '    = content()',
+        ].join('\n'),
+        options,
+        fixture.vm,
+      ),
+    );
 
     options.filename = '/views/forms/form.slm';
-    var src = [
-      '- extend("../layouts/app")',
-      'p Body from view'
-    ].join('\n');
+    var src = ['- extend("../layouts/app")', 'p Body from view'].join('\n');
 
     var result = fixture.template.render(src, {}, options, fixture.vm);
-    expect(result).toEqual('<html><head><title>Default title</title></head><body><p>Body from view</p></body></html>');
+    expect(result).toEqual(
+      '<html><head><title>Default title</title></head><body><p>Body from view</p></body></html>',
+    );
 
     var src2 = [
       '- extend("../layouts/app")',
       '= content("title")',
       '  title New title',
-      'p Body from view'
+      'p Body from view',
     ].join('\n');
 
     var result2 = fixture.template.render(src2, {}, options, fixture.vm);
-    expect(result2).toEqual('<html><head><title>New title</title></head><body><p>Body from view</p></body></html>');
+    expect(result2).toEqual(
+      '<html><head><title>New title</title></head><body><p>Body from view</p></body></html>',
+    );
   });
 
   test('test content append', function() {
     var options = {
-      basePath: '/views'
+      basePath: '/views',
     };
 
     options.filename = '/views/layouts/app.slm';
 
-    fixture.vm.cache(options.filename, fixture.template.exec([
-      'html',
-      '  head',
-      '    = content("title")',
-      '  body',
-      '    = content()'
-    ].join('\n'), options, fixture.vm));
+    fixture.vm.cache(
+      options.filename,
+      fixture.template.exec(
+        [
+          'html',
+          '  head',
+          '    = content("title")',
+          '  body',
+          '    = content()',
+        ].join('\n'),
+        options,
+        fixture.vm,
+      ),
+    );
 
     options.filename = '/views/forms/form.slm';
     var src = [
       '- extend("../layouts/app")',
       '= content("title", "append")',
       '  title 1',
-      'p Body from view'
+      'p Body from view',
     ].join('\n');
 
     var result = fixture.template.render(src, {}, options, fixture.vm);
-    expect(result).toEqual('<html><head><title>1</title></head><body><p>Body from view</p></body></html>');
+    expect(result).toEqual(
+      '<html><head><title>1</title></head><body><p>Body from view</p></body></html>',
+    );
 
     var src2 = [
       '- extend("../layouts/app")',
       '= content("title", "prepend")',
       '  title 2',
-      'p Body from view'
+      'p Body from view',
     ].join('\n');
 
     var result2 = fixture.template.render(src2, {}, options, fixture.vm);
-    expect(result2).toEqual('<html><head><title>2</title></head><body><p>Body from view</p></body></html>');
+    expect(result2).toEqual(
+      '<html><head><title>2</title></head><body><p>Body from view</p></body></html>',
+    );
   });
-
 
   test('test view loading', function() {
     var options = {
       basePath: __dirname + '/views',
-      filename: __dirname + '/views/index.slm'
+      filename: __dirname + '/views/index.slm',
     };
 
     var src = FS.readFileSync(options.filename, 'utf8');
@@ -232,7 +308,8 @@ describe('VMNode', function() {
     options.useCache = false;
     var fn3 = compile(src, options);
     var res3 = fn3({});
-    var expected = '<!DOCTYPE html><html><head><title>Nice</title><style type="text/css">body {background :red};</style></head><body><h1>Partial</h1><p>This is new footer</p></body><script>console.log(\'script\');</script><script type="text/javascript">console.log(\'javascript\');</script></html>';
+    var expected =
+      '<!DOCTYPE html><html><head><title>Nice</title><style type="text/css">body {background :red};</style></head><body><h1>Partial</h1><p>This is new footer</p></body><script>console.log(\'script\');</script><script type="text/javascript">console.log(\'javascript\');</script></html>';
     expect(res1).toEqual(expected);
     expect(res2).toEqual(expected);
     expect(res3).toEqual(expected);
@@ -240,7 +317,7 @@ describe('VMNode', function() {
 
   test('test resolvePath', function() {
     var options = {
-      filename: __dirname + '/views/index.slm'
+      filename: __dirname + '/views/index.slm',
     };
 
     var src = FS.readFileSync(options.filename, 'utf8');
@@ -248,11 +325,17 @@ describe('VMNode', function() {
     var compile = require('../lib/slm').compile;
 
     var fn1 = compile(src, options);
-    expect(function() { fn1({}); })
-      .toThrowError('the "basePath" option is required to use with "absolute" paths');
+    expect(function() {
+      fn1({});
+    }).toThrowError(
+      'the "basePath" option is required to use with "absolute" paths',
+    );
 
     var fn2 = compile(src, {});
-    expect(function() { fn2({}); })
-      .toThrowError('the "filename" option is required to use with "relative" paths');
+    expect(function() {
+      fn2({});
+    }).toThrowError(
+      'the "filename" option is required to use with "relative" paths',
+    );
   });
 });
